0fac96c00513c8f08afef5bbbfb0d64c413740b2
Tanay Kamath <2tanaykamath@gmail.com>
2020-11-21 16:39:55 +0530

    Tables populated with MySQL

--- src/sample/DAO/JDBCDao.java
+++ src/sample/DAO/JDBCDao.java
@@ -1,9 +1,10 @@
 package sample.DAO;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import DataClasses.Inventory;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.sql.*;
 
 public class JDBCDao {
     private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/jdbcdemo";
@@ -12,6 +13,7 @@ public class JDBCDao {
     private static final String INSERT_QUERY = "insert into registration (full_name, email_id, password) values (?, " +
             "?, ?)";
     private static final String SELECT_QUERY = "SELECT * FROM registration WHERE email_id = ? and password = ?";
+    private static final String SELECT_ROW_QUERY = "SELECT * FROM inventory";
 
     public void insertRecord(String fname, String email, String password) throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
@@ -45,5 +47,29 @@ public class JDBCDao {
         con.close();
         return false;
     }
+
+    public Inventory getRow() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+        Statement statement = con.createStatement();
+        ResultSet rs = statement.executeQuery(SELECT_ROW_QUERY);
+
+        boolean result = rs.next();
+
+        Inventory inventory = new Inventory();
+        inventory.setId(new SimpleIntegerProperty(rs.getInt(1)));
+        inventory.setName(new SimpleStringProperty(rs.getString(2)));
+        inventory.setPrice(new SimpleIntegerProperty(rs.getInt(3)));
+        inventory.setQuantity(new SimpleIntegerProperty(rs.getInt(4)));
+        inventory.setImage(new SimpleStringProperty(rs.getString(5)));
+        inventory.setDescription(new SimpleStringProperty(rs.getString(6)));
+        inventory.setCategory(new SimpleStringProperty(rs.getString(7)));
+
+        statement.close();
+        con.close();
+
+        return inventory;
+    }
+
 
 }
--- src/sample/HomeController.java
+++ src/sample/HomeController.java
@@ -6,11 +6,14 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import sample.DAO.JDBCDao;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -28,6 +31,10 @@ public class HomeController implements Initializable {
     public TableColumn<Inventory, String> descriptionCol;
     public TableColumn<Inventory, String> categoryCol;
 
+    public Button addBtn;
+    public Button updateBtn;
+    public Button deleteBtn;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         idCol.setCellValueFactory(new PropertyValueFactory<Inventory, Integer>("id"));
@@ -38,21 +45,38 @@ public class HomeController implements Initializable {
         descriptionCol.setCellValueFactory(new PropertyValueFactory<Inventory, String>("description"));
         categoryCol.setCellValueFactory(new PropertyValueFactory<Inventory, String>("category"));
 
-        inventoryTableView.setItems(getInventory());
+        try {
+            inventoryTableView.setItems(getInventory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public ObservableList<Inventory> getInventory() {
+    public ObservableList<Inventory> getInventory() throws Exception {
         ObservableList<Inventory> inventoryRow = FXCollections.observableArrayList();
-        Inventory inventory = new Inventory();
-        inventory.setId(new SimpleIntegerProperty(1));
-        inventory.setName(new SimpleStringProperty("Car"));
-        inventory.setPrice(new SimpleIntegerProperty(1000));
-        inventory.setQuantity(new SimpleIntegerProperty(12));
-        inventory.setImage(new SimpleStringProperty("No Image"));
-        inventory.setDescription(new SimpleStringProperty("JHEDVHEDHWDVW"));
-        inventory.setCategory(new SimpleStringProperty("Automobiles"));
+
+//        Inventory inventory = new Inventory();
+//        inventory.setId(new SimpleIntegerProperty(1));
+//        inventory.setName(new SimpleStringProperty("Car"));
+//        inventory.setPrice(new SimpleIntegerProperty(1000));
+//        inventory.setQuantity(new SimpleIntegerProperty(12));
+//        inventory.setImage(new SimpleStringProperty("No Image"));
+//        inventory.setDescription(new SimpleStringProperty("JHEDVHEDHWDVW"));
+//        inventory.setCategory(new SimpleStringProperty("Automobiles"));
+
+        JDBCDao jdbcDao = new JDBCDao();
+        Inventory inventory = jdbcDao.getRow();
         inventoryRow.add(inventory);
 
         return inventoryRow;
+    }
+
+    public void addProductEntry(ActionEvent event) {
+    }
+
+    public void updateProductEntry(ActionEvent event) {
+    }
+
+    public void deleteProductEntry(ActionEvent event) {
     }
 }
